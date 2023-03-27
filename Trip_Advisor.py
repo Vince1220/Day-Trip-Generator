71d91c1a04b56a2047e8949dd9f24923a152c270
Vince1220 <guitarvince025@yahoo.com>
2023-03-27 16:30:02 -0400

    Updated Trip_Advisor file

---
+++ Trip_Advisor.py
@@ -0,0 +1,30 @@
+destinations=['Downtown Miami','Palm Beach','Ft Lauderdale','Miami Beach']
+import random
+daily_destination= random.choice (destinations)
+
+
+
+
+restaurants=['Red Cow','Rock and Roll Ribs','Mint','News Cafe']
+import random
+daily_restaurant= random.choice (restaurants)
+
+
+transportations=['lyft','uber','metro rail','taxi']
+import random
+daily_transportation= random.choice (transportations)
+
+entertainments=['Arena Concert','Amphitheatre Concert','Arena Concert','Outdoor Concert']
+
+daily_entertainment= random.choice (entertainments)
+
+print (daily_destination,daily_entertainment,daily_transportation,daily_restaurant)
+input ('Are you satisfied with your trip?')
+#code works after debugging I generate randomized choices from each list and prints are you satisfied#
+input ('Is your trip complete?')
+print (daily_destination,daily_entertainment,daily_transportation,daily_restaurant)
+#after yes input to questions console prints verified confirmed choices# 
+
+
+
+
