3ae47d8facd9b4f9069d07edf79ef3e65fdb0e6a
Vince1220 <guitarvince025@yahoo.com>
2023-03-28 08:03:04 -0400

    updated commit

--- Trip_Advisor.py
+++ Trip_Advisor.py
@@ -2,13 +2,9 @@ destinations=['Downtown Miami','Palm Beach','Ft Lauderdale','Miami Beach']
 import random
 daily_destination= random.choice (destinations)
 
-
-
-
 restaurants=['Red Cow','Rock and Roll Ribs','Mint','News Cafe']
 import random
 daily_restaurant= random.choice (restaurants)
-
 
 transportations=['lyft','uber','metro rail','taxi']
 import random
@@ -19,12 +15,16 @@ entertainments=['Arena Concert','Amphitheatre Concert','Arena Concert','Outdoor 
 daily_entertainment= random.choice (entertainments)
 
 print (daily_destination,daily_entertainment,daily_transportation,daily_restaurant)
-input ('Are you satisfied with your trip?')
-#code works after debugging I generate randomized choices from each list and prints are you satisfied#
-input ('Is your trip complete?')
-print (daily_destination,daily_entertainment,daily_transportation,daily_restaurant)
+user_response=('yes')
+user_decline=('no')
+if user_response.lower() in user_response =='yes' :
+    input ('Are you satisfied with your trip? ')
+#code works after debugging I generate randomized choices from each list and prints are you satisfied#  
+if user_response.lower() in user_response=='yes' :
+    input ('Is your trip complete?')
+if user_response.lower() in user_response=='yes' :
+ print (daily_destination,daily_entertainment,daily_transportation,daily_restaurant)
 #after yes input to questions console prints verified confirmed choices# 
-
-
-
+if user_decline.lower() =='no':
+ input ('What feature would you like to change? Destinations?, Entertainments?, Transportations?,Restaurants?')
 
